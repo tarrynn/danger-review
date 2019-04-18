810a35fa89f64a35fc1d15e419850d7fc23335f1
tarrynn <catalin.gheonea@gmail.com>
2019-04-18 11:56:37 +0100

    added command to check if aws ec2 instance exists with the specific branch tag

--- lib/danger_review/plugin.rb
+++ lib/danger_review/plugin.rb
@@ -1,8 +1,15 @@
 module Danger
   class DangerReview < Plugin
 
-    def check
-      "none"
+    def check(branch = nil)
+      return if branch.nil?
+
+      review_exists = `aws ec2 describe-instances --filters 'Name=tag:branch,Values=#{branch}' | jq .Reservations | jq 'length'`.gsub("\n","").to_i
+      if review_exists
+        "https://#{branch}.review.bergamotte.com"
+      else
+        "none"
+      end
     end
 
   end
