8c0490dbe16a4abe5ac69ffef2fab67720d459ab
tarrynn <catalin.gheonea@gmail.com>
2019-04-19 11:53:38 +0100

    cleanup

--- lib/danger_review/plugin.rb
+++ lib/danger_review/plugin.rb
@@ -3,13 +3,23 @@ module Danger
 
     def check(branch = nil)
       return if branch.nil?
+      @branch = branch
+      check_review
+    end
 
-      review_exists = `aws ec2 describe-instances --filters 'Name=tag:branch,Values=#{branch}' | jq .Reservations | jq 'length'`.gsub("\n","").to_i
-      if review_exists
-        "[Available here](https://#{branch}.review.bergamotte.com)"
-      else
-        "none"
-      end
+    private
+
+    def check_review
+      markdown if ENV['REVIEW_ENVIRONMENT'] == "true"
+      markdown if aws_command
+    end
+
+    def aws_command
+      `aws ec2 describe-instances --filters 'Name=tag:branch,Values=#{@branch}' | jq .Reservations | jq 'length'`.gsub("\n","").to_i
+    end
+
+    def markdown
+      "[Available here](https://#{@branch}.review.bergamotte.com)"
     end
 
   end
